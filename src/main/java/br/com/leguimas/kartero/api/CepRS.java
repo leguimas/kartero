d5c71dca0868a82c87a5e891ddd7391b57eee806
Leandro Guimarães <leguimas@gmail.com>
2014-07-19 20:28:56 -0300

    teste no serviço REST

--- src/main/java/br/com/leguimas/kartero/api/CepRS.java
+++ src/main/java/br/com/leguimas/kartero/api/CepRS.java
@@ -6,13 +6,17 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import br.com.leguimas.kartero.cep.CEP;
+import br.com.leguimas.kartero.cep.CEPRepository;
 
 @Path("/cep")
 public class CepRS {
 
 	/**
 	 * Dado um CEP, retorna as informa&ccedil;&otilde;es do endere&ccedil;o atre&ccedil;ado ao CEP em quest&atilde;o.
-	 * 
+	 *
 	 * @summary Consulta CEP
 	 * @param cep
 	 *            O CEP a ser procurado. Voc&ecirc; pode informar o CEP em qualquer formato. No processamento,
@@ -25,6 +29,13 @@ public class CepRS {
 	@Path("/{cep}")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
 	public Response consultaCep(@PathParam("cep") String cep) {
-		return Response.ok(cep).build();
+		CEPRepository repositorio = new CEPRepository();
+		CEP cepEncontrado = repositorio.consultaCEP(cep);
+
+		if (cepEncontrado == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+
+		return Response.ok(cepEncontrado.emFormatoJson().toString()).build();
 	}
 }
--- src/test/java/br/com/leguimas/kartero/api/CepRSTest.java
+++ src/test/java/br/com/leguimas/kartero/api/CepRSTest.java
@@ -1,14 +1,18 @@
 package br.com.leguimas.kartero.api;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mortbay.jetty.HttpStatus;
 
@@ -16,13 +20,36 @@ import br.com.leguimas.kartero.TesteIntegrado;
 
 public class CepRSTest extends TesteIntegrado {
 
+	@BeforeClass
+	public static void carregaDadosDeTeste() throws IOException, SQLException {
+		TesteIntegrado.executaScript("cep.dados");
+	}
+
 	@Test
 	public void testaConsultaCEP() throws ClientProtocolException, IOException {
-
-		HttpClient requisicao = new DefaultHttpClient();
-		HttpResponse resposta = requisicao.execute(new HttpGet("http://localhost:8080/api/cep/13045135"));
-
+		HttpResponse resposta = realizaConsultaDeCEP("13045-135");
 		assertEquals(HttpStatus.ORDINAL_200_OK, resposta.getStatusLine().getStatusCode());
+
+		String conteudoDaResposta = EntityUtils.toString(resposta.getEntity());
+		assertTrue(conteudoDaResposta.startsWith("{"));
+		assertTrue(conteudoDaResposta.endsWith("}"));
+		assertTrue(conteudoDaResposta.contains("\"cep\":\"13045135\""));
+		assertTrue(conteudoDaResposta.contains("\"logradouro\":\"Rua Sargento Luis de Morais\""));
+		assertTrue(conteudoDaResposta.contains("\"bairro\":\"Jardim São Vicente\""));
+		assertTrue(conteudoDaResposta.contains("\"cidade\":\"Campinas\""));
+		assertTrue(conteudoDaResposta.contains("\"uf\":\"SP\""));
+	}
+
+	@Test
+	public void testaConsultaCEPInexistente() throws ClientProtocolException, IOException {
+		HttpResponse resposta = realizaConsultaDeCEP("13000-000");
+		assertEquals(HttpStatus.ORDINAL_404_Not_Found, resposta.getStatusLine().getStatusCode());
+	}
+
+	private HttpResponse realizaConsultaDeCEP(String cep) throws IOException, ClientProtocolException {
+		HttpClient requisicao = new DefaultHttpClient();
+		HttpResponse resposta = requisicao.execute(new HttpGet("http://localhost:8080/api/cep/" + cep));
+		return resposta;
 	}
 
 }
