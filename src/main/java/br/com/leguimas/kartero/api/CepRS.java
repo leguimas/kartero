249c3d5383f4a1aadd4c534bc1b9f8a60c0ac73f
Leandro Guimarães <leguimas@gmail.com>
2014-07-19 12:25:46 -0300

    configurando o gerador de documentação

--- src/main/java/br/com/leguimas/kartero/api/CepRS.java
+++ src/main/java/br/com/leguimas/kartero/api/CepRS.java
@@ -2,15 +2,29 @@ package br.com.leguimas.kartero.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/cep")
 public class CepRS {
 
+	/**
+	 * Dado um CEP, retorna as informa&ccedil;&otilde;es do endere&ccedil;o atre&ccedil;ado ao CEP em quest&atilde;o.
+	 * 
+	 * @summary Consulta CEP
+	 * @param cep
+	 *            O CEP a ser procurado. Voc&ecirc; pode informar o CEP em qualquer formato. No processamento,
+	 *            ser&atilde;o processados utilizados apenas os d&iacute;gitos num&eacute;ricos.
+	 * @return Dados do endereço de um CEP.
+	 * @statuscode 200 O CEP informado foi encontrado.
+	 * @statuscode 404 Foi informado um CEP n&atilde;o cadastrado na base de dados.
+	 */
 	@GET
-	@Path("/ping")
-	public Response ping() {
-		return Response.ok("pong").build();
+	@Path("/{cep}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+	public Response consultaCep(@PathParam("cep") String cep) {
+		return Response.ok(cep).build();
 	}
-
 }
